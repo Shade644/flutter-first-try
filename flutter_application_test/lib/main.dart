3464d790900bd32c2b2d2bccdd2eaa5a074f7a98
Adrian Włodarczyk <33695278+Shade644@users.noreply.github.com>
2023-03-19 16:41:43 +0100

    Day 1

--- flutter_application_test/lib/main.dart
+++ flutter_application_test/lib/main.dart
@@ -1,75 +1,44 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+home: Home(),
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+));
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
 
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Testing'),
-    );
-  }
-}
+class Home extends StatelessWidget {
+  const Home({super.key});
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+  appBar: AppBar(
+    title: Text('hello world'),
+   centerTitle: true,
+   backgroundColor: Color.fromARGB(255, 7, 100, 55),
+   
+  ),
+  body: Center(
+  //   child:Text('hello world',
+  //   style: TextStyle(
+  //     fontSize: 20,
+  //     fontWeight: FontWeight.bold,
+  //     letterSpacing: 2,
+  //     color: Colors.red,
+  //     fontFamily: 'RubikIso-Regular',      
+  //   ),
+  //   ),
 
-        title: Text(widget.title),
-      ),
-      body: Center(
-  
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Text(
-              'test'
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  child: Image.asset('assets/1.jpg')
+  ),
+  floatingActionButton: FloatingActionButton(
+    onPressed: () {  },
+    child: Text('click'),
+    backgroundColor: Colors.deepPurple,
+  ),
+);
+}
 }
